7bf6a1ac5d29250bb07578437556bb1d63414c04
Sohaib <sohaib.bilytica@gmail.com>
2025-08-28 17:23:38 +0500

    Initial commit

---
+++ ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -0,0 +1,24 @@
+using Serilog;
+
+namespace Ocelot.ApiGateway;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((env, config) =>
+            {
+                //config.AddJsonFile($"ocelot.{env.HostingEnvironment.EnvironmentName}.json", true, true);
+                config.AddJsonFile("ocelot.Local.json", true, true);
+
+
+            }).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            }).UseSerilog(Common.Logging.Logging.ConfigureLogger);
+}
